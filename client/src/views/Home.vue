daf8f078aa640208f8fddf4e7dedc8c54673b316
AhhhhhChiu <ahchiu9808@gmail.com>
2021-08-29 14:29:38 +0800

    feature: 引入elemui

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -6,13 +6,13 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { Options, Vue } from 'vue-class-component';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
-export default defineComponent({
-  name: 'Home',
+@Options({
   components: {
     HelloWorld,
   },
-});
+})
+export default class Home extends Vue {}
 </script>
